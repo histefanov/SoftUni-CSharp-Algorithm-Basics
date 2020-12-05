5dd8ccb68ec60239673e97f174e529ffc424ffae
Hristiyan Stefanov <christian.stefanov@outlook.com>
2020-12-05 22:58:51 +0200

    add simple search and sort algos

---
+++ Searching, Sorting and Greedy Algorithms - Lab/BinarySearch/Program.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Linq;
+
+namespace BinarySearch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+             var collection = Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToArray();
+            int targetNum = int.Parse(Console.ReadLine());
+
+            collection = collection.OrderBy(x => x).ToArray();
+
+            int targetNumIndex = RecursiveBinarySearch(collection, targetNum, 0, collection.Length - 1);
+
+            Console.WriteLine(targetNumIndex);
+        }
+
+        // Iterative approach:
+        private static int BinarySearch(int[] collection, int target)
+        {
+            var left = 0;
+            var right = collection.Length - 1;
+
+            while (left <= right)
+            {
+                var mid = (left + right) / 2;
+                var element = collection[mid];
+
+                if (element > target)
+                {
+                    right = mid - 1;
+                }
+                else if (element < target)
+                {
+                    left = mid + 1;
+                }
+                else
+                {
+                    return mid;
+                }
+            }
+
+            return -1;
+        }
+
+        // Recursive approach:
+        private static int RecursiveBinarySearch(int[] collection, int target, int leftBoundary, int rightBoundary)
+        {
+            int middleIdx = (leftBoundary + rightBoundary) / 2;
+
+            if (collection[middleIdx] == target)
+            {
+                return middleIdx;
+            }
+
+            if (leftBoundary > rightBoundary)
+            {
+                return -1;
+            }
+
+            if (collection[middleIdx] > target)
+            {
+                return RecursiveBinarySearch(collection, target, leftBoundary, middleIdx - 1);
+            }
+            else
+            {
+                return RecursiveBinarySearch(collection, target, middleIdx + 1, rightBoundary);
+            }
+        }
+    }
+}
---
+++ Searching, Sorting and Greedy Algorithms - Lab/SelectionSort/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Linq;
+
+namespace SelectionSort
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToArray();
+
+            SelectionSort(arr);
+            Console.WriteLine(string.Join(" ", arr));
+        }
+
+        private static void SelectionSort(int[] arr)
+        {
+            for (int i = 0; i < arr.Length; i++)
+            {
+                var minIdx = i;
+                for (int j = i + 1; j < arr.Length; j++)
+                {
+                    if (arr[j] < arr[minIdx])
+                    {
+                        minIdx = j;
+                    }
+                }
+
+                if (minIdx != i)
+                {
+                    int temp = arr[i];
+                    arr[i] = arr[minIdx];
+                    arr[minIdx] = temp;
+                }
+            }
+        }
+    }
+}
