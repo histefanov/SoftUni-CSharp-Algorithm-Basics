c53937f690133e32a85ef4b76245b1a6bdc94dd8
Hristiyan Stefanov <christian.stefanov@outlook.com>
2020-12-06 19:02:21 +0200

    add insertion sort

---
+++ Searching, Sorting and Greedy Algorithms - Lab/BubbleSort/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Linq;
+
+namespace BubbleSort
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var arr = Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToArray();
+
+            BubbleSort(arr);
+            Console.WriteLine(String.Join(" ", arr));
+        }
+
+        private static void BubbleSort(int[] arr)
+        {
+            bool needsSwapping = true;
+            while (needsSwapping)
+            {
+                needsSwapping = false;
+
+                for (int i = 0; i < arr.Length - 1; i++)
+                {
+                    if (arr[i + 1] < arr[i])
+                    {
+                        Swap(arr, i, i + 1);
+                        needsSwapping = true;
+                    }
+                }
+            }
+        }
+
+        private static void Swap(int[] arr, int firstIdx, int secondIdx)
+        {
+            var temp = arr[firstIdx];
+            arr[firstIdx] = arr[secondIdx];
+            arr[secondIdx] = temp;
+        }
+    }
+}
---
+++ Searching, Sorting and Greedy Algorithms - Lab/InsertionSort/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Linq;
+
+namespace InsertionSort
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var arr = Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToArray();
+
+            InsertionSort(arr);
+            Console.WriteLine(String.Join(" ", arr));
+        }
+
+        private static void InsertionSort(int[] arr)
+        {
+            for (int i = 1; i < arr.Length; i++)
+            {
+                var j = i;
+
+                while (j > 0 && arr[j] < arr[j - 1])
+                {
+                    Swap(arr, j, j - 1);
+                    j--;
+                }
+            }
+        }
+
+        private static void Swap(int[] arr, int j, int k)
+        {
+            var temp = arr[j];
+            arr[j] = arr[k];
+            arr[k] = temp;
+        }
+    }
+}
