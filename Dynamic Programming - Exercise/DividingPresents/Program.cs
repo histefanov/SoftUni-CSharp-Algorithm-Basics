8fa5f255300adc07f736daa701404d3b7dc4aa50
Hristiyan Stefanov <christian.stefanov@outlook.com>
2020-12-28 19:35:07 +0200

    add dynamic programming exercises

---
+++ Dynamic Programming - Exercise/DividingPresents/Program.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DividingPresents
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var presents = Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToArray();
+
+            var lesserSumTarget = presents.Sum() / 2;
+
+            var sums = GetAllSums(presents);
+
+            var adamsSum = sums
+                .Where(s => s.Key <= lesserSumTarget)
+                .OrderByDescending(s => s.Key)
+                .FirstOrDefault()
+                .Key;
+
+            List<int> adamsPresents = RecoverPresents(adamsSum, sums);
+
+            var result = new StringBuilder();
+            result
+                .AppendLine($"Difference: {presents.Sum() - adamsSum * 2}")
+                .AppendLine($"Alan:{adamsSum} Bob:{presents.Sum() - adamsSum}")
+                .AppendLine($"Alan takes: {string.Join(" ", adamsPresents)}")
+                .Append("Bob takes the rest.");
+
+            Console.WriteLine(result);
+        }
+
+        private static List<int> RecoverPresents(int sum, Dictionary<int, int> sums)
+        {
+            var presents = new List<int>();
+
+            while (sum > 0)
+            {
+                var currentPresent = sums[sum];
+                presents.Add(currentPresent);
+                sum -= currentPresent;
+            }
+
+            return presents;
+        }
+
+        private static Dictionary<int, int> GetAllSums(int[] presents)
+        {
+            var sums = new Dictionary<int, int> { { 0, 0 } };
+
+            foreach (var present in presents)
+            {
+                var currentSums = sums.Keys.ToArray();
+                foreach (var sum in currentSums)
+                {
+                    var newSum = sum + present;
+
+                    if (!sums.ContainsKey(newSum))
+                    {
+                        sums.Add(newSum, present);
+                    }
+                }
+            }
+
+            return sums;
+        }
+    }
+}
---
+++ Dynamic Programming - Exercise/Dynamic Programming - Exercise/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Dynamic_Programming___Exercise
+{
+    class BinomalCoefficient
+    {
+        public int N { get; set; }
+
+        public int K { get; set; }
+
+        public long Value { get; set; }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var n = int.Parse(Console.ReadLine());
+            var k = int.Parse(Console.ReadLine());
+            var memo = new List<BinomalCoefficient>();
+
+            long result = Binom(n, k, memo);
+
+            Console.WriteLine(result);
+        }
+
+        private static long Binom(int n, int k, List<BinomalCoefficient> memo)
+        {
+            var memoizedCoefficient = memo.FirstOrDefault(x => x.N == n && x.K == k);
+
+            if (memoizedCoefficient != null)
+            {
+                return memoizedCoefficient.Value;
+            }
+
+            if (k == 0 || n == k)
+            {
+                return 1;
+            }
+
+            long result = Binom(n - 1, k, memo) + Binom(n - 1, k - 1, memo);
+            memo.Add(new BinomalCoefficient() { N = n, K = k, Value = result });
+
+            return result;
+        }
+    }
+}
