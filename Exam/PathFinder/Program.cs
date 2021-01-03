4268b169e6d37e85ca6cfe8c1089ba2239ca0e70
Hristiyan Stefanov <christian.stefanov@outlook.com>
2021-01-03 18:52:34 +0200

    add exam

---
+++ Exam/PathFinder/Program.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace PathFinder
+{
+    class Program
+    {
+        private static bool[] visited;
+
+        static void Main(string[] args)
+        {
+
+            int n = int.Parse(Console.ReadLine());
+            List<int>[] graph = ReadGraph(n);
+
+            int p = int.Parse(Console.ReadLine());
+            CheckPaths(graph, p);
+        }
+
+        private static void CheckPaths(List<int>[] graph, int p)
+        {
+            for (int i = 0; i < p; i++)
+            {
+                visited = new bool[graph.Length];
+
+                int[] input = Console.ReadLine()
+                    .Split()
+                    .Select(int.Parse)
+                    .ToArray();
+
+                Queue<int> path = new Queue<int>();
+
+                foreach (var node in input)
+                {
+                    path.Enqueue(node);
+                }
+
+                bool result = BFS(graph, path);
+                Console.WriteLine(result ? "yes" : "no");
+            }
+        }
+
+        private static bool BFS(List<int>[] graph, Queue<int> path)
+        {
+            var source = path.Dequeue();
+
+            var BFSQueue = new Queue<int>();
+            
+            BFSQueue.Enqueue(source);
+            visited[source] = true;
+
+            while (BFSQueue.Count > 0 && path.Count > 0)
+            {
+                var currentNode = BFSQueue.Dequeue();
+                var nextPathNode = path.Peek();
+                
+                foreach (var child in graph[currentNode])
+                {
+                    if (!visited[child] && child == nextPathNode)
+                    {
+                        BFSQueue.Enqueue(child);
+                        path.Dequeue();
+                        visited[child] = true;
+                        break;
+                    }
+                }
+            }
+
+            return path.Count == 0;
+        }
+
+        private static List<int>[] ReadGraph(int n)
+        {
+            var result = new List<int>[n];
+
+            for (int i = 0; i < n; i++)
+            {
+                var input = Console.ReadLine();
+
+                if (String.IsNullOrWhiteSpace(input))
+                {
+                    result[i] = new List<int>();
+                }
+                else
+                {
+                    var children = input
+                        .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                        .Select(int.Parse)
+                        .ToArray();
+
+                    result[i] = new List<int>(children);
+                }
+            }
+
+            return result;
+        }
+    }
+}
---
+++ Exam/Socks/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Linq;
+
+namespace Socks
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] socks1 = Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToArray();
+
+            int[] socks2 = Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToArray();
+
+            int lcs = GetLCS(socks1, socks2);
+            Console.WriteLine(lcs);
+        }
+
+        private static int GetLCS(int[] socks1, int[] socks2)
+        {
+            var lcsTable = new int[socks1.Length + 1, socks2.Length + 1];
+
+            for (int r = 1; r < lcsTable.GetLength(0); r++)
+            {
+                for (int c = 1; c < lcsTable.GetLength(1); c++)
+                {
+                    if (socks1[r - 1] == socks2[c - 1])
+                    {
+                        lcsTable[r, c] = lcsTable[r - 1, c - 1] + 1;
+                    }
+                    else
+                    {
+                        lcsTable[r, c] = Math.Max(lcsTable[r - 1, c], lcsTable[r, c - 1]);
+                    }
+                }
+            }
+
+            return lcsTable[lcsTable.GetLength(0) - 1, lcsTable.GetLength(1) - 1];
+        }
+    }
+}
